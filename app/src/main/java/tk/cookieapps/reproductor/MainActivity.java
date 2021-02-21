ad8196ef900f7b1cb23e3c3a6e21f357b957c816
Mauricio Medrano Machicado <maumedmac@gamail.com>
2021-02-21 19:47:09 -0400

    Audio

--- app/src/main/java/tk/cookieapps/reproductor/MainActivity.java
+++ app/src/main/java/tk/cookieapps/reproductor/MainActivity.java
@@ -2,7 +2,10 @@ package tk.cookieapps.reproductor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.copia);
+        Button play = findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+
+            }
+        });
+        Button stop = findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
     }
 }
